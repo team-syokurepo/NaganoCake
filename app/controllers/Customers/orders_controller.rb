cd79d56e5f1bb46505b3bfc64288040fc23146dc
river-spring <rivermspring@gmail.com>
2020-08-02 13:50:59 +0900

    [Add]customers/orders・customers/products・admins/productsにそれぞれcontroller/viewを追加

--- app/controllers/Customers/orders_controller.rb
+++ app/controllers/Customers/orders_controller.rb
@@ -1,2 +1,20 @@
 class Customers::OrdersController < ApplicationController
+	def index
+		
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def confirm
+	end
+
+	def thanks
+	end
 end
