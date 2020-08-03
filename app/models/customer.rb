3c6662324d421493f2243966bd656996feffa248
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-08-03 11:45:34 +0000

    fix customer validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,7 +7,10 @@ class Customer < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :address_lists, dependent: :destroy
 
-  validates :name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
   validates :encrypted_password, presence: true
 
   acts_as_paranoid
