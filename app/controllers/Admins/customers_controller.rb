fa587d075812b4c03de1244e673d99f0f663cc3c
vagrant <vagrant@localhost.localdomain>
2020-08-04 14:29:48 +0000

    [start:admin customer]

--- app/controllers/Admins/customers_controller.rb
+++ app/controllers/Admins/customers_controller.rb
@@ -1,8 +1,8 @@
 class Admins::CustomersController < ApplicationController
-
+ # before_action customer_admin, only: [:index, :customer_restore]
 
 	def index
-		
+		@customers = Customer.with_deleted
 	end
 
 	def show
@@ -17,6 +17,14 @@ class Admins::CustomersController < ApplicationController
 		
 	end
 
-
+private
+    def customer_admin
+       @customers = Customer.with_deleted
+       # if  current_customer.admin == false
+       #     redirect_to /
+       # else
+       #     render action: "index"
+       # end
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,3 @@ Category.create(name: "ケーキ" , valid_flag: true)
 Category.create(name: "焼き菓子" , valid_flag: true)
 Category.create(name: "プリン" , valid_flag: true)
 Category.create(name: "キャンディ" , valid_flag: true)
-
-
-
-
