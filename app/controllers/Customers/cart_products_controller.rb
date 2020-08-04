5255a704e108f36584d99f41211b3ff27942a798
river-spring <rivermspring@gmail.com>
2020-08-04 17:05:16 +0900

    [Add]商品カテゴリ検索機能とカートへ追加機能

--- app/controllers/Customers/cart_products_controller.rb
+++ app/controllers/Customers/cart_products_controller.rb
@@ -5,14 +5,20 @@ class Customers::CartProductsController < ApplicationController
 	end
 
 	def create
-		
+		cart_product =  CartProduct.new(cart_product_params)
+		cart_product.customer_id = current_customer.id
+		byebug
+		if cart_product.save
+			redirect_to customers_cart_products_path
+		else
+			render("coustomers/products/show")
+		end
 	end
 
 	def destroy
 		cart_product = CartProduct.find(params[:id])
 		cart_product.destroy
 		redirect_to request.referer
-
 	end
 
 	def update
@@ -29,19 +35,23 @@ class Customers::CartProductsController < ApplicationController
 	end
 
 private
+	def cart_product_params
+		params.require(:cart_product).permit(:product_id, :quantity, :customer_id)
+	end
 
-def current_cart
-	# current_cart = CartProduct.find_by(id: session[cart_product_id])
-	# current_cart = CartProduct.create unless current_cart
-	# session[:cart_product_id] = current_cart.id
-	# current_cart
-	if session[:cart_product_id]
-		cart_product = Cart.find(session[:cart_product_id])
-end
+	def current_cart
+		# current_cart = CartProduct.find_by(id: session[cart_product_id])
+		# current_cart = CartProduct.create unless current_cart
+		# session[:cart_product_id] = current_cart.id
+		# current_cart
+		if session[:cart_product_id]
+			cart_product = Cart.find(session[:cart_product_id])
+	end
 
 
 
-def cart_product_params
-	params.require(:cart_product).permit(:cart_products.product)
+# def cart_product_params
+# 	params.require(:cart_product).permit(:cart_products.product)
+# end
 end
 end
--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
   def top
+  	@categories = Category.all
   end
 
   def about
