98b73e857d17c520289884fc2fafe1e334cb9102
river-spring <rivermspring@gmail.com>
2020-08-06 13:47:10 +0900

    [Fix]注文ステータスのデータ型変・管理者ログイン後の遷移画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:postal_code,:address,:phone_number,:status])
   end
+
+  def after_sign_in_path_for(resource)
+  	case resource
+	when Admin
+		admins_orders_today_path
+	when Customer
+	 	root_path
+  	end
+  end
 end
